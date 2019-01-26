cf7192dd7e30ebe8749fa8adbf5d5bffebe3539b
Chandrajeet Choudhary <chandrajeet.c16@iiits.in>
2019-01-26 22:17:16 +0530

    jwt token added

--- abhisarga2k19/urls.py
+++ abhisarga2k19/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('UserAuth.urls')),
     # url(r'^auth/', include('social_django.urls', namespace='social')),
+    url(r'^api-token-auth/', obtain_jwt_token),
 ]
