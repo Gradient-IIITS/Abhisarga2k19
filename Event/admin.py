d15bf042780250e003d1b8ce5b3fc7ac87f0fce9
Chandrajeet Choudhary <chandrajeet.c16@iiits.in>
2019-03-08 23:29:37 +0530

    admin

--- Event/admin.py
+++ Event/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Event, Team, Member, EventCatalogue
+from .models import Event, Team, Member, EventCatalogue, EventCategory
 
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(Team)
 admin.site.register(Member)
 admin.site.register(EventCatalogue)
+admin.site.register(EventCategory)
