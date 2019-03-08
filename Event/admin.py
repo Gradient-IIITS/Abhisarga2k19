b170d82232263ab96251035196f43743a99de42b
Chandrajeet Choudhary <chandrajeet.c16@iiits.in>
2019-03-08 22:58:49 +0530

    home url updated

--- Event/admin.py
+++ Event/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event, Team, Member, EventCatalogue
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Team)
+admin.site.register(Member)
+admin.site.register(EventCatalogue)
