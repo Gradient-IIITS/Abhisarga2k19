fb8e766bae6c519322bb345019f113d128452bf4
Chandrajeet Choudhary <chandrajeet.c16@iiits.in>
2019-03-11 04:14:30 +0530

    staticfiles

--- staticfiles/assets/js/events.js
+++ staticfiles/assets/js/events.js
@@ -14,3 +14,45 @@ function changeselect(param){
   document.getElementById('eventselect').value = param.value;
   changecontent();
 }
+
+function initialize(){
+  var rules = document.getElementsByClassName('event-rules');
+  for(i=0; i<rules.length; i++){
+    rules[i].addEventListener('click', function(){
+      this.parentElement.parentElement.getElementsByClassName('modal-rules')[0].style.display = "block";
+    });
+  }
+  var register = document.getElementsByClassName('event-register');
+  for(i=0; i<register.length; i++){
+    register[i].addEventListener('click', function(){
+      this.parentElement.parentElement.getElementsByClassName('modal-register')[0].style.display = "block";
+    });
+  }
+  var modals = document.getElementsByClassName('modal');
+  for(i=0; i<modals.length; i++){
+    modals[i].addEventListener('click', function(event){
+      if(event.target == this){
+        this.style.display = "none";
+      }
+    });
+  }
+  var closebutton = document.getElementsByClassName('modal-close');
+  for(i=0; i<closebutton.length; i++){
+    closebutton[i].addEventListener('click', function(){
+      this.parentElement.parentElement.style.display = "none";
+    });
+  }
+  var checkbox = document.getElementsByClassName('register-checkbox');
+  for(i=0; i<checkbox.length; i++){
+    checkbox[i].addEventListener('click', function(){
+      var actual = this.parentElement.parentElement.getElementsByClassName('register-agree')[0];
+      if(actual.checked == true){
+        actual.checked = false;
+        this.style.color = "rgba(0,0,0,0)";
+      }else{
+        actual.checked = true;
+        this.style.color = "rgba(255,255,255,1)";
+      }
+    });
+  }
+}
