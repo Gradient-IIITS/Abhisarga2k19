a67a552ac328d7be441d700cd5dc718664fef85c
Chandrajeet Choudhary <chandrajeet.c16@iiits.in>
2019-03-10 19:02:53 +0530

    event registration

--- Event/urls.py
+++ Event/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import event
+from .views import event, eventRegistration
 
 app_name = "Event"
 
 urlpatterns = [
 	url(r'^$', event, name='events'),
+	url(r'^eventRegistration/$', eventRegistration, name='eventRegistration'),
 	]
--- Event/views.py
+++ Event/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.utils.crypto import get_random_string
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth import authenticate, login, logout
 
-from .models import EventCategory, Event
+from .models import EventCategory, Event, Team
 # Create your views here.
 
 def event(request):
@@ -16,4 +16,30 @@ def event(request):
 	for _ in event_cat:
 		events = Event.objects.filter(event_category__id=_.id)
 		all_events.append({"category":_, "events":events})
-	return render(request, event_template, {"event_category":event_cat, "all_events":all_events})
+	participated_events = Team.objects.filter(leader__username=request.user.username)
+	part = list()
+	for obj in participated_events:
+		part.append(obj.event)
+	# print(part)
+	return render(request, event_template, {"event_category":event_cat, "all_events":all_events, "participated_events":part})
+
+
+def eventRegistration(request):
+	if request.method=='GET':
+		event_id = request.GET.get('event_id')
+		check = Team.objects.filter(leader__username=request.user.username, event__id=event_id)
+		if len(check)==0:
+			team = Team()
+			team.team_name = request.user.username
+			team.event = Event.objects.get(id=event_id)
+			team.leader = request.user
+			team.save()
+		else:
+			for _ in check:
+				_.delete()
+
+		return HttpResponseRedirect(reverse('Event:events'))
+
+
+def teamEventRegistration(request):
+	pass
