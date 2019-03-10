c9f547a3ae58d8e65e3a800df3f020bccaf26714
Chandrajeet Choudhary <chandrajeet.c16@iiits.in>
2019-03-10 19:22:52 +0530

    contact page added

--- static/assets/js/events.js
+++ static/assets/js/events.js
@@ -4,7 +4,7 @@ function changecontent(){
   list = document.getElementById('listnav').getElementsByTagName('li');
   events = document.getElementsByClassName('events-internal');
   list[selected].classList.remove("active-link");
-  events[selected].style.display = "none";
+  events[selected] .style.display = "none";
   selected = parseInt(document.getElementById('eventselect').value);
   list[selected].classList.add("active-link");
   events[selected].style.display = "block";
